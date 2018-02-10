de9ff43ac863a9a224b5051c5a5adac1bed84a26
ben tan <btan@esri.com>
2018-02-11 00:26:21 +0800

    update add layer

--- addlayer_test.py
+++ addlayer_test.py
@@ -20,11 +20,17 @@ y_data = np.square(x_data) - 0.5 + noise
 xs = tf.placeholder(tf.float32, [None, 1])
 ys = tf.placeholder(tf.float32, [None, 1])
 
-# l1 = add_layer(
+l1 = add_layer(xs, 1, 10, activate_function=tf.nn.relu)
+prediction = add_layer(l1, 10, 1, activate_function=None)
+loss = tf.reduce_mean(tf.reduce_sum(tf.square(ys-prediction), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
 
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
-for i in range(100):
-    print(sess.run(output_data))
+for i in range(1000):
+    sess.run(train_step, feed_dict={xs:x_data, ys:y_data})
+    if i % 50==0:
+        print(sess.run(loss, feed_dict={xs:x_data, ys:y_data}))
